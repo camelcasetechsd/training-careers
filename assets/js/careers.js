39c89ff2415eccb9f2e3767b45469ca9ffb8b6b6
ahmedelbougha <ahmed.elbougha@gmail.com>
2016-01-06 15:50:46 +0200

    TC2

--- assets/js/careers.js
+++ assets/js/careers.js
@@ -14,11 +14,11 @@ $(document).ready(function () {
 
             $(obj).each(function () {
                 // warning for exceeding 180 hours duration for a career
-                this.total_duration > '180:00:00' ? $color = "#FF0000" : $color = "#FFFFFF";
+                this.total_duration > 10800 ? $color = "#FF0000" : $color = "#FFFFFF";
 
                 // appending div to wrap div 
                 $('#wrap').append('<div class="box"><div class="innerContent" id="' + this.id + '" ><h2 class="ass">' + this.name + '</h2>\n\
-                <h3 style="color:' + $color + ';" >' + this.total_duration + '</h3></div></div>');
+                <h3 style="color:' + $color + ';" >' + this.total_duration_formatted + '</h3></div></div>');
             });
 
             // redirecting to outlines page
--- lib/Model/Career.php
+++ lib/Model/Career.php
@@ -22,10 +22,9 @@ class Career
 
         if (isset($this->connection)) {
             if (empty($id)) {
-                return $this->connection->select("ASSOCIATIVE", "career","id, name, SEC_TO_TIME(total_duration*60) as total_duration");
+                return $this->connection->select("ASSOCIATIVE", "career","id, name, SEC_TO_TIME(total_duration*60) as total_duration_formatted, total_duration");
             } else {
-                return $this->connection->select("ASSOCIATIVE", "career","name, SEC_TO_TIME(total_duration*60) as total_duration",FALSE,"id", $id, "=");
-               
+                return $this->connection->select("ASSOCIATIVE", "career","name, SEC_TO_TIME(total_duration*60) as total_duration_formatted, total_duration",FALSE,"id", $id, "=");
             }
         }
         else {
