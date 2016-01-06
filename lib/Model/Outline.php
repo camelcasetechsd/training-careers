bfa1f9beb112ddc6cd6579a7c62eb369460ece92
ahmedReda1 <se_ahmedreda@yahoo.com>
2016-01-06 12:15:23 +0200

    issue #3 fixing codes

--- lib/Model/Outline.php
+++ lib/Model/Outline.php
@@ -62,7 +62,28 @@ class Outline
     {
 
         if (isset($this->connection)) {
-            return $this->connection->select("ASSOCIATIVE", "outline ol JOIN course crs ON ol.course_id = crs.id", "ol.*", FALSE, "category_id", $category_id, "=");
+            
+            $requirements = array(
+                'code',
+                'category_id'
+            );
+            
+            $outlines = $this->connection->select("ASSOCIATIVE", "outline ol JOIN course crs ON ol.course_id = crs.id", "ol.*", FALSE, "category_id", $category_id, "=");
+        
+            for ($i = 0; $i < count($outlines); $i++) {
+                // adding course code to outlines object
+                $courseData = $this->connection->select("ASSOCIATIVE", "course", $requirements, FALSE, "id", $outlines[$i]['course_id'], "=");
+                $outlines[$i]['course_code'] = $courseData[0]['code'];
+                // adding category code to outlines object
+                $categoryData = $this->connection->select("ASSOCIATIVE", "category", "code", FALSE, "id", $courseData[0]["category_id"], "=");
+                $outlines[$i]['category_code'] = $categoryData[0]['code'];
+                // adding array of careers for each outline
+                $careerData = $this->connection->select("ASSOCIATIVE", "career_outlines", "career_id", FALSE, "outline_id", $outlines[$i]['id'], "=");
+                $outlines[$i]['careers'] = $careerData;
+            }
+            
+            
+            return $outlines;
         }
         else {
             return "Database connection Error";
