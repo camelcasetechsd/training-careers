8f36d09085de3bf1af984a96eae79f1e69a2d089
ahmedelbougha <ahmed.elbougha@gmail.com>
2016-02-03 09:37:39 +0200

    TC10 - Unify the design

---
+++ public/front/lib/Include/layout.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>ThinkCSI Training Careers</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- Le styles -->
+        <link href="/css/bootstrap.min.css" media="screen" rel="stylesheet" type="text/css">
+        <link href="/css/bootstrap-theme.min.css" media="screen" rel="stylesheet" type="text/css">
+        <link href="/css/style.css" media="screen" rel="stylesheet" type="text/css">
+        <link href="/img/favicon.ico" rel="shortcut icon" type="image/vnd.microsoft.icon">
+        <!-- Scripts -->
+        <!--[if lt IE 9]><script type="text/javascript" src="/js/html5shiv.min.js"></script><![endif]-->
+        <!--[if lt IE 9]><script type="text/javascript" src="/js/respond.min.js"></script><![endif]-->
+        <script type="text/javascript" src="/js/jquery.min.js"></script>
+        <script type="text/javascript" src="/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+            <div class="container">
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="/"><img src="/img/thinkcsi-logo.png" alt="ThinkCSI" width="61" />&nbsp; Training Careers</a>
+                </div>
+                <div class="collapse navbar-collapse">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="/">Home</a></li>
+                        <li class="active"><a href="/front/application">Front</a></li>
+                        <li class="active"><a href="/admin/career">Career</a></li>
+                        <li class="active"><a href="/admin/category">Category</a></li>
+                        <li class="active"><a href="/admin/course">Course</a></li>
+                        <li class="active"><a href="/admin/skill">Skill</a></li>
+                    </ul>
+                </div><!--/.nav-collapse -->
+            </div>
+        </nav>
+        <div class="container">
+
+            <?php echo $content; ?>
+            <hr>
+            <footer>
+
+            </footer>
+        </div> <!-- /container -->
+    </body>
+</html>
